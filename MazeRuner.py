a957d03dd94899faa29f6f2f031513542e4a2636
Pramod Koshti <pramodkoshti7389@gmail.com>
2019-02-28 10:31:26 +0530

    Update MazeRuner.py

--- MazeRuner.py
+++ MazeRuner.py
@@ -6,79 +6,78 @@ wn = turtle.Screen()
 wn.bgcolor("black")
 wn.title("Maze Runner")
 wn.setup(700,700)
-
+possible_direcetion = ["Left","Up","Right","Down"]
 
 turtle.register_shape("Maze_Block.gif")
 turtle.register_shape("player.gif")
 turtle.register_shape("Enter.gif")
 turtle.register_shape("Exit.gif")
 
+
 class Pen(turtle.Turtle):    
     def __init__(self):        
         turtle.Turtle.__init__(self)
-        self.shape("square")
-        self.color("white")
         self.penup()
-        self.speed(0)
+        self.speed(10)
 
 class Player(turtle.Turtle):    
     def __init__(self):        
         turtle.Turtle.__init__(self)
         self.shape("player.gif")
-        self.color("blue")
         self.penup()
         self.speed(1)
-        self.direction = random.choice(["Left","Right","Up","Down"])
-   
-    def Move(self):
-        if(self.direction == "Left"):
-            dx = -24
-            dy = 0
-        elif(self.direction == "Right"):
-            dx = 24
-            dy = 0
-        elif(self.direction == "Up"):
-            dx = 0
-            dy = 24
-        elif(self.direction == "Down"):
-            dx = 0
-            dy = -24     
-        else:
-            dx = 0
-            dy = 0
-        
-        move_to_x = self.xcor() + dx        
-        move_to_y = self.ycor() + dy
-        
-        if(move_to_x,move_to_y) not in walls:
-            self.goto(move_to_x,move_to_y)
-        else:
-            self.direction = random.choice(["Left","Right","Up","Down"])
-        self.Move()
-        
+        self.direction = "Left"
+        self.current_direction = possible_direcetion[0]       
+    
+    
     def go_up(self):
         move_to_x = self.xcor()
         move_to_y = self.ycor() + 24
-        if(move_to_x,move_to_y) not in walls:
-             self.goto(move_to_x,move_to_y)
+        if(move_to_x,move_to_y) not in walls:            
+            self.goto(move_to_x,move_to_y)
+            self.direction = "Left"                        
+        else:
+            self.direction = "Right"
+        self.Move()
         
     def go_down(self):
         move_to_x = self.xcor()
         move_to_y = self.ycor() - 24
-        if(move_to_x,move_to_y) not in walls:
-             self.goto(move_to_x,move_to_y)
+        if(move_to_x,move_to_y) not in walls:            
+            self.goto(move_to_x,move_to_y)
+            self.direction = "Right"
+        else:
+            self.direction = "Down"
+        self.Move()
         
     def go_right(self):
         move_to_x = self.xcor() + 24
         move_to_y = self.ycor()
         if(move_to_x,move_to_y) not in walls:
-             self.goto(move_to_x,move_to_y) 
+            self.direction = "Up"
+            self.goto(move_to_x,move_to_y)               
+        else:
+            self.direction = "Down"
+        self.Move()
         
     def go_left(self):
         move_to_x = self.xcor() -24
         move_to_y = self.ycor()
         if(move_to_x,move_to_y) not in walls:
-             self.goto(move_to_x,move_to_y)                
+            self.goto(move_to_x,move_to_y)                        
+        else:
+            self.direction = "Up"
+        self.Move()
+        
+    def Move(self):
+        if(self.direction == "Left"):                        
+            self.go_left()
+        elif(self.direction == "Up"):
+            self.go_up()
+        elif(self.direction == "Right"):
+            self.go_right()
+        else:            
+            self.go_down()                            
         
 class In(turtle.Turtle):    
     def __init__(self):        
@@ -96,31 +95,31 @@ class Out(turtle.Turtle):
         
 levels = [""]
 
-level_1 = ["XIXXXXXXXXXXXXXXXXXXXXXXX",
-           "XP              XXXXXXXXX",
-           "X      XXXX       XX  XXX",
-           "XX  XX XXX      X XXXXXX",
-           "XXX     XXXXX X X  XX XX",
-           "XXX XX XXXXXXX     XXXXX",
-           "XXX XXX  XXXXX   XXXXXXXX",
+level_1 = ["XXXXXXXXXXXXXXXXXXXXXXXXX",
+           "X              X XXXXXXXX",
+           "X      XXXX             O",
+           "XXX XX XXX       X XXXXXX",
+           "X       XXXXX X  X  XX XX",
+           "X X XX XXXXXXX      XXXXX",
+           "X X XXX  XXXXX   XXXXXXXX",
            "X        XXXXXX XX   XXXX",
            "X X X XX XXXXXX X   XXXXX",
            "X XXX XX        XXX XXXXX",
-           "XX  X XXX  XX XX XX XXXXX",
-           "XXX X    XX      XX XXXXX",
-           "XXX X XXX    XXX   XXXXXX",
+           "X   X XXX  XX XX XX XXXXX",
+           "X X X    XX      XX XXXXX",
+           "X X X XXX    XXX   XXXXXX",
            "X  XX XXXX       XXXXXXXX",
            "X X X XXX  XXXXX  XXXXXXX",
            "X  XX XXXX XXXX  XXX    X",
            "XX           XXX XXXXXXXX",
            "XX XX XXXXXX XXX XXXXXXXX",
            "XX XX  XXXXX  XX     XXXX",
-           "XX  XX  X XXX    XX  XXXX",
+           "XX  XX  X XXXX   XX  XXXX",
            "XX  XX XXXXX   XXXX  XXXX",
-           "X  XXX  XXXXXX X XX  XXXX",
-           "XXXXXX   XXXXX      XXXXX",
-           "XXXX       XXXXXXXX XXXXX",
-           "XXXXXXXXXXXXXXXXXXXOXXXXX"
+           "X  XXX       X X XX    XX",
+           "X   XX   XXX X      X   X",
+           "XX         X           PX",
+           "XXXXXXXXXXXXXXXXXXXXXXXIX"
           ]
 levels.append(level_1)
 
